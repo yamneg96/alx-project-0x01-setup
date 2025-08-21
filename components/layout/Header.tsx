8fd3eae3cb5a1fd91e62b5c68563fe5c7679dd4b
Yamlak Negash <yamlaknegash96@gmail.com>
2025-08-21 04:17:46 +0300

    Added Header and updated home UI also simple routing from the home page.

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import Link from 'next/link'; 
 
-const Header : React.FC = () => {
+const Header: React.FC = () => {
   return (
-    <div>Header</div>
-  )
+    <header className="bg-blue-600 text-white shadow-md py-4">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        <h3 className="font-bold text-2xl">
+     <Link href="/">Daily <span className='bg-gradient-to-r from-white to-blue-300 bg-clip-text text-transparent'>Contents</span></Link>
+  </h3>
+        <nav>
+          <ul className="flex space-x-6">
+            <li className="hover:underline">
+              <Link href="/posts">Posts</Link>
+            </li>
+            <li className="hover:underline">
+              <Link href="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
